3f064324c00fb5303b6813850fe110e327765d45
SussieFrantzen <cph-sf252@stud.ek.dk>
2025-12-04 14:31:24 +0100

    oprettet classe Oderline lavet på Producct+ Order construktor og getter/setter og sat abstract class

--- src/Product.java
+++ src/Product.java
@@ -1,16 +1,17 @@
-public class Product {
+public abstract class Product {
     // instans variabel
     protected String name;
     protected int dbNo;
     protected String location;
 
-    Product(int dbNo, String name, String location) {  // construktor
+    Product(int dbNo, String name, String location) {  // constructor
         this.dbNo = dbNo;
         this.name = name;
         this.location = location;
     }
 
-
-
-
+    Product(String name, int dbNo) {
+        this.name = name;
+        this.dbNo = dbNo;
+    }
 }
