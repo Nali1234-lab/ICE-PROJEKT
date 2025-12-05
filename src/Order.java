02fd37b2391b9e872919fbd6ed2c1f66966f62d5
SussieFrantzen <cph-sf252@stud.ek.dk>
2025-12-05 09:59:37 +0100

    Order er ændret på en instantsvariabel fået konstruktor og products datafile har fået data ind

--- src/Order.java
+++ src/Order.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
+
 public class Order {
     private String dato;
     private long orderNo;
     private long customerNo;
-    private int quanityOrdered;
+    private ArrayList<String> ordreLines; // changed from int quanityOrdered to Arraylist ordreLines
 
 
-// construktor
+    // constructor
+    public Order(long customerNo, String dato, long orderNo, ArrayList<String> ordreLines) {
+        this.customerNo = customerNo;
+        this.dato = dato;
+        this.orderNo = orderNo;
+        this.ordreLines = ordreLines;
+    }
 
     // OrdreList()
     // metode som kalder på aflæsning af fil og laver et arraylist med de ønskede data
